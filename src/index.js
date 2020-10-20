f02c0f7bdb65a36c8b33cc7d38f3ea7f6330a811
waka87 <a.shaumenova87@gmail.com>
2020-10-20 23:44:17 +0600

    init: start task morse decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    var decodedStr = '';
+var strResult = '';
+var a;
+
+    for (var i=0; i<expr.length; i= i+10) {
+      str = expr.slice(i, i+10);
+      decodedStr = '';
+      for (var j=0; j<10; j=j+2) {
+        if (str[j] === '1' && str[j+1] === '0') {
+          decodedStr = decodedStr + '.';
+        } else if (str[j] === '1' && str[j+1] === '1') {
+          decodedStr = decodedStr + '-';
+        } 
+      }
+      if (decodedStr !== '') {
+        a = MORSE_TABLE[decodedStr];
+      } else {
+        a = ' ';
+      }
+      strResult = strResult + a;
+    }
+    return strResult;
 }
 
 module.exports = {
